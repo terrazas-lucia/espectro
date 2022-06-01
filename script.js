ea7b99f7688fee472a7f2276553f422350cb992a
Lucía Belén Terrazas <luciabelenterrazas@gmail.com>
2022-06-01 01:47:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,20 +10,44 @@ function PlaySound(melody) {
 
 function showClouds(){
   var x = document.getElementById("burbujitas");
-  var y = document.getElementById("color-changing");
-  x.addEventListener("click", showMoreClouds);
+ /*  x.addEventListener("click", showMoreClouds, changeColor, changeColor2, changeColor3, changeColor4, changeColor5, changeColor6); */
   if(x.style.display === 'none'){
     x.style.display = "block";
-    y.style.backgroundColor = x.style.backgroundColor;
-    
   } else{
     x.style.display = "none";
   }
 
 }
 
-function showMoreClouds(){
-   document.getElementById("mas-burbujitas").style.display += "block";
+function showMoreClouds(n){
+    var e = document.getElementById("mas-burbujitas" + n);
+    if(e.style.display === 'none'){
+      e.style.display = 'block';
+    } else{
+      e.style.display = 'none';
+    }
 }
 
+function changeColor(){
+  document.body.style.backgroundColor = '#ADB5BD';
+}
 
+function changeColor2(){
+  document.body.style.backgroundColor = '#6C757D';
+}
+
+function changeColor3(){
+  document.body.style.backgroundColor = '#495057';
+}
+
+function changeColor4(){
+  document.body.style.backgroundColor = '#343A40';
+}
+
+function changeColor5(){
+  document.body.style.backgroundColor = '#212529';
+}
+
+function changeColor6(){
+  document.body.style.backgroundColor = 'black';
+}
